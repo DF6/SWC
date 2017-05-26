f8cc0fc3eb3ff1a67f352a7ac15d419a4197b89c
Axel Alonso <axjunu@hotmail.com>
2017-05-26 14:16:56 +0200

    Traspaso de código entre controladores
    Se realiza el traspaso de código entre el controlador indexCtrl y el controlador appCtrl con tal de tener todo el código ordenado en un archivo .js

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
-var index=angular.module('index',['ngRoute']);
-index.config(function($routeProvider){
+var appIni=angular.module('appIni',['ngRoute']);
+appIni.config(function($routeProvider){
   $routeProvider
-            .when("/", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "ini.html"})
-            .when("/primera", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "primera.html"})
-            .when("/copa", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "copa.html"})
-            .when("/champions", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "champions.html"})
-            .when("/segunda", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "segunda.html"})
-            .when("/europaleague", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "europaleague.html"})
-            .when("/intertoto", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "intertoto.html"})
-            .when("/plantilla", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "plantilla.html"})
-            .when("/supercopaeuropa", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "supercopaeuropa.html"})
-            .when("/supercopaclubes", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "supercopaclubes.html"})
-            .when("/pendientes", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "pendientes.html"})
-            .when("/resumenpartido", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "resumenpartido.html"})
-            .when("/introducirresultado", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "introducirresultado.html"})
-            .when("/elegirtipo", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "eleccionquest.html"})
-            .when("/questcena", {controller: "indexCtrl",controllerAs: "vm",templateUrl: "questcena.html"});
+            .when("/", {controller: "appCtrl",controllerAs: "vm",templateUrl: "ini.html"})
+            .when("/primera", {controller: "appCtrl",controllerAs: "vm",templateUrl: "primera.html"})
+            .when("/copa", {controller: "appCtrl",controllerAs: "vm",templateUrl: "copa.html"})
+            .when("/champions", {controller: "appCtrl",controllerAs: "vm",templateUrl: "champions.html"})
+            .when("/segunda", {controller: "appCtrl",controllerAs: "vm",templateUrl: "segunda.html"})
+            .when("/europaleague", {controller: "appCtrl",controllerAs: "vm",templateUrl: "europaleague.html"})
+            .when("/intertoto", {controller: "appCtrl",controllerAs: "vm",templateUrl: "intertoto.html"})
+            .when("/plantilla", {controller: "appCtrl",controllerAs: "vm",templateUrl: "plantilla.html"})
+            .when("/supercopaeuropa", {controller: "appCtrl",controllerAs: "vm",templateUrl: "supercopaeuropa.html"})
+            .when("/supercopaclubes", {controller: "appCtrl",controllerAs: "vm",templateUrl: "supercopaclubes.html"})
+            .when("/pendientes", {controller: "appCtrl",controllerAs: "vm",templateUrl: "pendientes.html"})
+            .when("/resumenpartido", {controller: "appCtrl",controllerAs: "vm",templateUrl: "resumenpartido.html"})
+            .when("/introducirresultado", {controller: "appCtrl",controllerAs: "vm",templateUrl: "introducirresultado.html"})
+            .when("/elegirtipo", {controller: "appCtrl",controllerAs: "vm",templateUrl: "eleccionquest.html"})
+            .when("/questcena", {controller: "appCtrl",controllerAs: "vm",templateUrl: "questcena.html"});
 });
-index.controller("navindex", function($location){
+appIni.controller("navCtrl", function($location){
         var map = this;
         map.estoy = function(ruta){
             return $location.path() == ruta;
         }
-})
-index.controller("indexCtrl",function(indexFactory, $http){
+    })
+appIni.controller("appCtrl",function(indexFactory,$http){
   var uq=this;
-  uq.preguntas=indexFactory.getPreguntas();
-  uq.user=indexFactory.user;
+  uq.user=indexFactory.getUser();
 
   uq.login = function(){
-    angular.forEach(uq.usuarios, function(value, key){
+    angular.forEach(uq.users, function(value, key){
       if(value.user == uq.user.user)
       {
         if(value.pass == uq.user.pass)
         {
-          uq.user.valido = true;
-          uq.user.idEquipo = value.id;
-          uq.user.nombreEquipo = uq.getEquipoById(value.id);
+          uq.user.valid = true;
+          uq.user.teamID = value.id;
+          uq.user.teamName = uq.getTeamById(value.id);
         }
       }
     });
+    if(!uq.user.valid){
+      uq.user.user = '';
+      uq.user.pass = '';
+      Materialize.toast('Usuario o contraseña inválida');
+    }
     return uq.user.valido;
   }
 });
-index.factory("indexFactory", function(){
-    var user={id:-1, user: '', pass: '', valido: false, nombreEquipo: '', idEquipo: -1};
+appIni.factory("indexFactory", function(){
+    var user={id:-1, user: '', pass: '', valid: false, teamName: '', teamID: -1};
     var interfaz = {
         pass:"",
         getUser: function(){
             return user;
         },
         setUser: function(newUser){
-          user=newUser;
+          user = newUser;
         }
     }
     return interfaz;
